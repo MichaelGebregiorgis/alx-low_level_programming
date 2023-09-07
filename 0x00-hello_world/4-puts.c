7506355c64f473cf09fd0b142cf907911e848c8b
Your Name <you@example.com>
2023-09-07 01:10:38 -0700

    Using puts to print out the text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - display char using puts
  *
- * display char using puts
- * Return: Always 0 (Success)
+ * Return:0(Success)
  */
 int main(void)
-{
-	int puts(const char, "\"Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
