4a6a87f6bb151486f595bf08f3f62c391033394c
Your Name <you@example.com>
2023-09-08 16:11:40 -0700

    Print all values of hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints out all the hexadecimal values
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char af;
+	int num;
+
+	for (num = '1'; num <= '9'; num++)
+	{
+		putchar(num);
+	for (af = 'a'; af <= 'f'; af++)
+	{
+		putchar(af);
+	}
+	putchar('\n');
+	return (0);
+}
