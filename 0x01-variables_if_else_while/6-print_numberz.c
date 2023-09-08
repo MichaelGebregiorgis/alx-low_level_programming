fa47647a63255d788a1f9ff7931ea7fcfa097960
Your Name <you@example.com>
2023-09-08 15:49:43 -0700

    Print numbers without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - print out numbers from 0 to 9 without using char syntax
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
