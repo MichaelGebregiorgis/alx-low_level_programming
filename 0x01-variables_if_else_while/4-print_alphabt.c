214780650635b650f2073e98f530509f0787645d
Your Name <you@example.com>
2023-09-08 15:31:33 -0700

    Prints all alphabets with exceptions

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,12 @@ int main(void)
 {
 	char az = 'a';
 
-	while (az != 'e' && az != 'q')
+	while (az < 'z')
 	{
-		putchar(az);
-		az++;
+		if (az != 'e' && az != 'q')
+		{
+			putchar(az);
+		}
 	}
 	putchar('\n');
 	return (0);
