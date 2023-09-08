813fca2ec7ecbf7ec50063b0288fd4225189b1d6
Your Name <you@example.com>
2023-09-08 15:24:31 -0700

    Prints all alphabets with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints out all alphabets except e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char az = 'a';
+
+	while (az != e && az != q)
+	{
+		putchar(az);
+		az++;
+	}
+	putchar('\n');
+	return (0);
+}
