c24153f10907197f9dc2459c982a169b1a644199
Your Name <you@example.com>
2023-09-08 15:39:16 -0700

    Print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - List numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
