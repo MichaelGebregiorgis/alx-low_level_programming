4d8c0b20c23efb2ac5a67fc1503de8b5df8b481e
Your Name <you@example.com>
2023-09-08 17:25:50 -0700

    Different combinations of two digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,8 +17,10 @@ int main(void)
 	{
 		for (j = '1'; j <= '9'; j++)
 		{
-			putchar(i);
-			putchar(j);
+			if (i < j){
+				putchar(i);
+				putchar(j);
+			}
 			if (i < 8 || j < 9)
 			{
 				putchar(',');
