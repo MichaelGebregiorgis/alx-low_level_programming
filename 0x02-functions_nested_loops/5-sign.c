698acbde9b703cb5f6dc64a1d6a07c9d4231f3c0
Michael Gebregiorgis <you@example.com>
2023-09-13 06:58:51 -0700

    Checks the signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,20 +15,20 @@ int print_sign(int n)
 		_putchar('+');
 		_putchar(',');
 		_putchar(' ');
-		_putchar(1);
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		_putchar(0);
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
-		_putchar('/');
+		return (-1);
 	}
 }
