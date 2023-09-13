b918ceedda609536b38936b93b271237768b18ae
Michael Gebregiorgis <you@example.com>
2023-09-13 10:49:27 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,22 +23,21 @@ void times_table(void)
 				l = (mul - k) / 10;
 				_putchar(l + '0');
 				_putchar(k + '0');
-				if (j < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar('\n');
-				}
 			}
 			else
 			{
 				_putchar(mul + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
+
+			if (j < 10)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                }
+                                else
+                                {
+                                        _putchar('\n');
+                                }
 		}
 	}
 }
