d2214a4160bc5fdc80996e791b02a02d76a3b669
Michael Gebregiorgis <you@example.com>
2023-09-13 11:10:01 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ *@n: data type
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	if (n > 97)
+	{
+		n--;
+	}
+	else if (n < 99)
+	{
+		n++;
+	}
+	return (n);
+	_putchar(',');
+	_putchar(' ');
+}
