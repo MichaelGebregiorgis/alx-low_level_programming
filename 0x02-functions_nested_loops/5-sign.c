f71e7798a7882e7808112c4256f07ca8a8183fbf
Michael Gebregiorgis <you@example.com>
2023-09-13 06:55:26 -0700

    Checks the signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks sign of the numbers
+ *
+ * @n: data type
+ *
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('/');
+	}
+}
