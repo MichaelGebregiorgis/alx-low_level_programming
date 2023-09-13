f09bdd0fb6b4a341124ffa696a6a8367271e68e2
Michael Gebregiorgis <you@example.com>
2023-09-13 12:24:42 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d ", n);
+				printf("%d", n);
 				printf("\n");
 				break;
 			}
@@ -31,8 +31,8 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d ", n);
-				_putchar('\n');
+				printf("%d", n);
+				printf("\n");
 				break;
 			}
 			else
