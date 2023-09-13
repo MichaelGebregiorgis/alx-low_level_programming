7586d8131f504d32ca34a5a8d4eef546cd6848b2
Michael Gebregiorgis <you@example.com>
2023-09-13 06:37:40 -0700

    Checks for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if the value is an alphabet or not
+ *
+ * @c: data type
+ *
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	return (0);
+}
