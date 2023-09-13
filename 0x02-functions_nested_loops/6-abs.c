14b4ec0a334eb18d3c5c8a7256d98471a7ec7879
Michael Gebregiorgis <you@example.com>
2023-09-13 07:16:18 -0700

    Operates absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Compute absolute values
+ *
+ * @num: data type
+ * Return: 0
+ */
+
+int _abs(int num)
+{
+	int asbval;
+
+	absval = abs(num);
+	_putchar(absval);
+}
