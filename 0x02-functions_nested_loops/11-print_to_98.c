8149d0a601fbc233f745dbe656273d2c43c226a4
Michael Gebregiorgis <you@example.com>
2023-09-13 11:28:19 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,20 @@ void print_to_98(int n)
 	if (n > 97)
 	{
 		n--;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(',');
 	}
 	else if (n < 99)
 	{
 		n++;
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(',');
+		_putchar(' ');
 	}
 	else
 	{
 		n = n;
 	}
-	_putchar(n / 10 + '0');
-	_putchar(n % 10 + '0');
-	_putchar(',');
-	_putchar(' ');
 }
