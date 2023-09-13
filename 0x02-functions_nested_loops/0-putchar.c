611f5eed35b46539e0175334ef6286348ef44163
Michael Gebregiorgis <you@example.com>
2023-09-12 22:14:43 -0700

    Print out _putchar string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Focal entry
+ *
+ * Description - Program that prints out the string _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
