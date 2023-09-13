cc94db8e60a3c966b67c6513b18d411c361f1abd
Michael Gebregiorgis <you@example.com>
2023-09-13 09:26:41 -0700

    24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	for (h = 0; h <= 5; h++)
+	for (h = 0; h <= 23; h++)
 	{
-		for (m = 0; m <= 9; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
