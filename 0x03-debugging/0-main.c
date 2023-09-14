5434caf854a9f2809e8159cb981f0d3963487276
Michael Gebregiorgis <you@example.com>
2023-09-13 23:56:54 -0700

    checks if positive negative or zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - checks if the number is positive, negative or zero
+ * 
+ * @i: data type
+ *
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative", i);
+	}
+	else
+	{
+		printf("%d is zero", i);
+	}
+	return (0);
+}
