faaf8ba43c63b138403b7c878e7f92b080d365f7
Michael Gebregiorgis <you@example.com>
2023-09-14 00:35:01 -0700

    sign checker

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,16 @@
 #include "main.h"
 
 /**
- * positive_or_negative - checks if the number is positive, negative or zero
- *
- * @i: data type
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i > 0)
-	{
-		printf("%d is positive", i);
-	}
-	else if (i < 0)
-	{
-		printf("%d is negative", i);
-	}
-	else
-	{
-		printf("%d is zero", i);
-	}
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
 }
