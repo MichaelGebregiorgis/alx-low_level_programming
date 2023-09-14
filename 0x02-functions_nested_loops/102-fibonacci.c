e308322c16c9a14450977db2414891fbc00a4295
Michael Gebregiorgis <you@example.com>
2023-09-14 11:09:08 -0700

    Print out the fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - the first 50 Fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+	int sum;
+	int i;
+
+	num1 = 1;
+	num2 = 2;
+	i = 0;
+	sum = 0;
+
+	while (i < 50)
+	{
+		sum = num1 + num2;
+		printf("%d", sum);
+		num1 = num2;
+		num2 = sum;
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+}
