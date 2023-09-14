8029c45d4ca49a2ca9c142465cf6f935e1d2bbea
Michael Gebregiorgis <you@example.com>
2023-09-14 10:56:19 -0700

    Add all numbers divisible by 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print out all multiples of three or five
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum;
+	int num;
+	int i;
+
+	sum = 0;
+	i = 0;
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum = sum + i;
+		}
+		i++;
+	}
+	printf("%d\n", sum);
+}
