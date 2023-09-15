f274c4655411e6e2e30cc32abb3e4283e23a6f61
Michael Gebregiorgis <you@example.com>
2023-09-15 02:12:58 -0700

    Checks for uppercase characters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <ctype.h>
 
 /**
  * _isupper - Checks if the input is uppercase or not
@@ -10,18 +11,8 @@
 
 int _isupper(int c)
 {
-	if (isupper(c) == 1)
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
-		_putchar(isupper(c));
-	}
-	else if (isupper(c) == 0)
-	{
-		_putchar(c);
-		_putchar(':');
-		_putchar(' ');
-		_putchar(isupper(c));
-	}
+	_putchar(c);
+	_putchar(':');
+	_putchar(' ');
+	_putchar(isupper(c));
 }
