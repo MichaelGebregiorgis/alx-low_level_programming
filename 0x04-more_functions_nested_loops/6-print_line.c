042daa7fbfaacfc602c7fff42644c3b18c99aef0
Michael Gebregiorgis <you@example.com>
2023-09-14 22:45:42 -0700

    Print _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - print _ n times
+ *
+ * @n: Data type
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int iTimes;
+
+	if (n > 0)
+	{
+		for (iTimes = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
