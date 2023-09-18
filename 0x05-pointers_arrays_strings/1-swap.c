fb7a19025218c07763388e8a17b591c3a7435443
Michael Gebregiorgis <you@example.com>
2023-09-18 06:32:12 -0700

    Swap two int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swap values of two integer
+ *
+ * @a: Data type
+ * @b: Data type
+ *
+ * Return: 0
+ */
+
+swap_int(int *a, int *b)
+{
+	int init;
+
+	init = *a;
+	*a = *b;
+	*b = init;
+}
