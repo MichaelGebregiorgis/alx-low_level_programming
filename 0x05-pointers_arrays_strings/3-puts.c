5c86027219954b7dbb9424184953429dd33bcd17
Michael Gebregiorgis <you@example.com>
2023-09-18 06:55:13 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Print string using putchar
+ *
+ * @str: Data type
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i <= strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
