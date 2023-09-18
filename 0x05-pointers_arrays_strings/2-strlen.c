072459d62e089f8ab557093bcd6c60b9cb9dc1c9
Michael Gebregiorgis <you@example.com>
2023-09-18 06:40:33 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: Data type
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (; *s++)
+	{
+		lenght++;
+	}
+	return (lenght);
+}
