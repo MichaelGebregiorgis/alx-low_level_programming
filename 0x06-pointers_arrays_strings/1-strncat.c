79757ef22cc2fa108c3b0f4023d4c2b52d20ef2e
Michael Gebregiorgis <you@example.com>
2023-09-20 23:45:32 -0700

    Concatenate using n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate strings
+ *
+ * @dest: Data type
+ * @src: Data type
+ * @n: data type
+ *
+ * Return: 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int str1 = 0;
+	int str2;
+
+	for (dest[str1] != '\0')
+	{
+		str1 += 1;
+	}
+	for (str2 = 0; str2 < n;)
+	{
+		for (; src[str2] != '\0')
+		{
+			dest[str1] = src[str2];
+			str1 += 1;
+			str2 += 1;
+		}
+	}
+	dest[str1] = '\0';
+	return (dest);
+}
