23d88ecb1b4226f11b106935d117a63f07db2ed2
Michael Gebregiorgis <you@example.com>
2023-09-21 08:06:23 -0700

    encode using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 -  Encodes a string using rot13
+ *
+ * @c: Data type
+ *
+ * Return: 0
+ */
+
+char *rot13(char *c)
+{
+	int inc = 0;
+
+	for (; c[inc] != '\0'; i++)
+	{
+		while ((c[inc] >= 'a' && c[inc] <= 'z') ||
+				(c[inc] >= 'A' && c[inc] <= 'Z'))
+		{
+			if ((c[inc] >= 'a' && c[inc] <= 'm') ||
+					(c[inc] >= 'A' && c[inc] <= 'M'))
+			{
+				c[inc] += 13;
+			}
+			else
+			{
+				c[inc] -= 13;
+			}
+			inc++;
+		}
+	}
+	return (c);
+}
