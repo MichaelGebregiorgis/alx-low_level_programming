3e3bb6dbc7dd3d38426128fa5565351b2b27d139
Michael Gebregiorgis <you@example.com>
2023-09-20 23:49:03 -0700

    Concatenate using n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,13 +15,13 @@ char *_strncat(char *dest, char *src, int n)
 	int str1 = 0;
 	int str2;
 
-	for (dest[str1] != '\0')
+	for (; dest[str1] != '\0';)
 	{
 		str1 += 1;
 	}
 	for (str2 = 0; str2 < n;)
 	{
-		for (; src[str2] != '\0')
+		for (; src[str2] != '\0';)
 		{
 			dest[str1] = src[str2];
 			str1 += 1;
