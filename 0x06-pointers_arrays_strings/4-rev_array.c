325c0fb27668cde2118daf55afb46a06f4da2127
Michael Gebregiorgis <you@example.com>
2023-09-21 01:25:16 -0700

    Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - array reversor
+ *
+ * @a: Data type
+ * @n: Data type
+ *
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int b;
+	int c;
+
+	for (b = 0; b < n - 1; b++)
+	{
+		c = a[b];
+		a[b] = a[n];
+		a[n] = c;
+	}
+}
