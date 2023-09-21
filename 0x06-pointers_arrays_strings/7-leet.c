7b893a6e3b28066242ae310f92effd08f2726660
Michael Gebregiorgis <you@example.com>
2023-09-21 07:33:58 -0700

    Encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet -  encodes a string into 1337
+ *
+ * @c: Data type
+ *
+ * Return: 0
+ */
+
+char *leet(char *c)
+{
+	int inc;
+	int j;
+
+	char c1[] = "aAeEoOtTlL";
+	char c2[] = "001122334477";
+
+	for (i = 0; c[inc] != '\0'; inc++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[inc] == c1[j])
+			{
+				c[inc] = c2[j];
+			}
+		}
+	}
+	return (c);
+}
