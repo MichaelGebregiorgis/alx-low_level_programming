3a88c78960b6e83e54768d673a90b2f5845ac53c
Michael Gebregiorgis <you@example.com>
2023-09-22 09:27:39 -0700

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,8 +13,8 @@ char *leet(char *c)
 	int inc;
 	int j;
 
-	char c1[] = "aAeEoOtTlL";
-	char c2[] = "0011334477";
+	char *c1[] = "aAeEoOtTlL";
+	char *c2[] = "4433007711";
 
 	for (inc = 0; c[inc] != '\0'; inc++)
 	{
