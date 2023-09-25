b81f0e9c29332e5e24845d042f7519d93578becd
Michael Gebregiorgis <you@example.com>
2023-09-25 03:14:21 -0700

    memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy = copies memory area
+ *
+ * @dest: Data type
+ *
+ * @n: Data type
+ *
+ * @src: Data type
+ *
+ * Return: pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int inc;
+
+	for (inc = 0; inc < n; inc++)
+	{
+		dest[inc] = src[inc];
+	}
+
+	return (dest);
+}
