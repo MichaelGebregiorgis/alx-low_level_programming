60b2effe75492aa76982ae57706db29758cc9762
Michael Gebregiorgis <you@example.com>
2023-09-29 00:47:35 -0700

    Power function

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power function sing two numbers
+ *
+ * @x: Data type
+ *
+ * @y: Data type
+ *
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
