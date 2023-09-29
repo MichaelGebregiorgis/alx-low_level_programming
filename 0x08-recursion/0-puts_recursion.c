9802763a4b199074d98911f0a43e10848e8546a5
Michael Gebregiorgis <you@example.com>
2023-09-29 00:03:04 -0700

    puts function recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,10 @@
 
 void _puts_recursion(char *s)
 {
-	int inc = 0;
-
-	if (s[inc] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
-	_putchar(s[inc + 1]);
+	_putchar(*s);
+	_puts_recursion(*s + 1);
 }
