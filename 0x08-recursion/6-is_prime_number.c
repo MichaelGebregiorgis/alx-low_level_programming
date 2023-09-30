32043d79c7f3e8e9e73b7a8863bb03e37f6f2ffc
Michael Gebregiorgis <you@example.com>
2023-09-30 14:31:34 -0700

    Check for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int recureVal(int n, int num);
 /**
  * is_prime_number - chack if the number is prime
  *
@@ -10,4 +11,24 @@
 
 int is_prime_number(int n)
 {
-	
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (recureVal(n, n - 1));
+}
+
+int recureVal(int n, int num)
+{
+	if (num == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	else if (n % num == 0 && num > 0)
+	{
+		_putchar('0');
+		return;
+	}
+	return (recureVal(n, num - 1));
+}
