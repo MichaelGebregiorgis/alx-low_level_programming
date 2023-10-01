8e2849c73f6a921490fffcc1f7594925bc46dcb3
Michael Gebregiorgis <you@example.com>
2023-09-30 22:59:13 -0700

    check for identical

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,11 +13,11 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0' && *s1 == '\0')
 	{
-		return (0);
+		return (1);
 	}
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 	{
-		return (0);
+		return (1);
 	}
 	if (*s2 == '*')
 	{
