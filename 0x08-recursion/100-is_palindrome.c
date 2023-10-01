29f2e1091e243bb9e23c1502fe457f431720ab5a
Michael Gebregiorgis <you@example.com>
2023-09-30 22:36:06 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+int strLen(char *s);
+int pointer(char *s, int i);
+
+/**
+ * strLen - length
+ *
+ * @s: Data type
+ *
+ * Return: 0
+ */
+
+int strLen(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + strLen(++s));
+}
+
+/**
+ * pointer - pointer to a string
+ *
+ * @s: Data type
+ *
+ * @i: Data type
+ *
+ * Return: 0
+ */
+
+int pointer(char *s, int i)
+{
+	if (i < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + 1))
+	{
+		return (pointer(s + 1, i - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - checks for words that are read the same way backwards
+ *
+ * @s: Data type
+ *
+ * Return: 0
+ */
+
+int is_palindrome(char *s)
+{
+	int length = strLen(s);
+
+	return (pointer(s, length - 1));
+}
