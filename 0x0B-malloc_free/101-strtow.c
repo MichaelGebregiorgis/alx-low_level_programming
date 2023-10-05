b7cdddf24ba53a620117cfbb898594c57a831461
Michael Gebregiorgis <you@example.com>
2023-10-05 01:03:28 -0700

    string split

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * wordC - counter
+ *
+ * @c: Data type
+ *
+ * Return: 0
+ */
+
+int wordC(char *c)
+{
+	int a = 0, inc = 0, out = 0;
+
+	while (c[inc] != '\0')
+	{
+		if (s[c] == ' ')
+		{
+			a = 0;
+		}
+		else if (a == 0)
+		{
+			a = 1;
+			out++;
+		}
+		inc++;
+	}
+	return (out);
+}
+
+/**
+ * strtow - splits a string into words
+ *
+ * @str: Data type
+ *
+ * Return: 0
+ */
+
+char **strtow(char *str)
+{
+	char **val, *temp;
+	int inc = 0, i = 0, len = 0, chars, a = 0, strt, end;
+
+	for (; *(str + len;))
+		len++;
+	chars = wordC(str);
+	if (chars == 0)
+		return (NULL);
+	val = malloc(sizeof(char *) * (chars + 1));
+	if (val == NULL)
+		return (NULL);
+	while (i <= len)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+			if (a)
+			{
+				end = i;
+				temp = malloc(sizeof(char) * (c + 1));
+				if (temp == NULL)
+				{
+					return (NULL);
+				}
+				while (start < end)
+				{
+					*temp++ = str[start++];
+				}
+				*temp = '\0';
+				val[inc] = temp - a;
+				inc++;
+				a = 0;
+
+			}
+		else if (a++ == 0)
+		{
+			start = i;
+		}
+		i++;
+	}
+	val[a] = NULL;
+	return (val);
+}
