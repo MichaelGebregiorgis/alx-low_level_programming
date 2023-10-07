3462d915e7868a8d13ee070dde6174e16ae36eb2
Michael Gebregiorgis <you@example.com>
2023-10-07 08:29:45 -0700

    add +ve number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,39 +13,39 @@ int sti(char *c)
 {
 	int inc = 0, d = 0, val = 0, len = 0, e = 0, dig = 0;
 
-    for (; c[len] != '\0';)
-    {
-        len += 1;
-    }
+	for (; c[len] != '\0';)
+	{
+		len += 1;
+	}
 
-    while (inc < len && e == 0)
-    {
-        if (c[inc] == '-')
-        {
-            ++d;
-        }
-        if (c[inc] >= '0' && c[inc] <= '9')
-        {
-            dig = c[inc] - '0';
-            if (d % 2)
-            {
-                dig = -dig;
-            }
-            val *= 10 + dig;
-            e = 1;
-            if (c[inc + 1] < '0' || c[inc + 1] > '9')
-            {
-                break;
-            }
-            e = 0;
-        }
-        inc += 1;
-    }
-            if (e == 0)
-        {
-                return (0);
-        }
-        return (val);
+	while (inc < len && e == 0)
+	{
+		if (c[inc] == '-')
+		{
+			++d;
+		}
+		if (c[inc] >= '0' && c[inc] <= '9')
+		{
+			dig = c[inc] - '0';
+			if (d % 2)
+			{
+				dig = -dig;
+			}
+			val *= 10 + dig;
+			e = 1;
+			if (c[inc + 1] < '0' || c[inc + 1] > '9')
+			{
+				break;
+			}
+			e = 0;
+		}
+		inc += 1;
+	}
+	if (e == 0)
+	{
+		return (0);
+	}
+	return (val);
 }
 /**
  * main - adder
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
 	while (inc1 < argc)
 	{
-		for (;argv[inc1][inc2] != '\0'; inc2++)
+		for (; argv[inc1][inc2] != '\0'; inc2++)
 		{
 			if (argv[inc1][inc2] > '9' || argv[inc1][inc2] < '0')
 			{
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 		}
 		inc1++;
 	}
-	while (m <argc)
+	while (m < argc)
 	{
 		n = sti(argv[m]);
 		if (n >= 0)
