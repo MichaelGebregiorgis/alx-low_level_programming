f48257d92c715fecf92aaf560ea8e6360d379e21
Michael Gebregiorgis <you@example.com>
2023-10-12 14:25:22 -0700

    numbers then new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (inc < n)
 	{
 		printf("%d", va_arg(lst, int));
-		if (inc != n - 1 && separator != NULL)
+		if (inc != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
