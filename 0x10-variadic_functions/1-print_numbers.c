09a79e8406f43d40c8461d8a429e0b0ebea4113a
Michael Gebregiorgis <you@example.com>
2023-10-12 14:21:26 -0700

    numbers then new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ *
+ * @separator: Data type
+ *
+ * @n: Data type
+ *
+ * Return: 0
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list lst;
+	unsigned int inc = 0;
+
+	va_start(lst, n);
+
+	while (inc < n)
+	{
+		printf("%d", va_arg(lst, int));
+		if (inc != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		inc++;
+	}
+	printf("\n");
+	va_end(lst);
+}
