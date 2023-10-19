efaf23757a14157396b6e238cd0df53634193e3a
Michael Gebregiorgis <you@example.com>
2023-10-19 11:14:23 -0700

    list elements

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,23 @@
 #ifndef lists_h
 #define lists_h
 
+/**
+ * struct list_val - linked list
+ *
+ * @len: Data type
+ *
+ * @str: Data type
+ *
+ * @next: Data type
+ */
+
+typedef struct list_val
+{
+	char *str;
+	unsigned int len;
+	struct list_val *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
