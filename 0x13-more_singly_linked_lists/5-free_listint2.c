99a5258b61777ffe2330cd2b8f80deb096413ce1
Michael Gebregiorgis <you@example.com>
2023-10-24 06:01:06 -0700

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (!head)
+	if (head == NULL)
 	{
 		return;
 	}
@@ -19,5 +19,5 @@ void free_listint2(listint_t **head)
 		free(*head);
 		*head = tmp;
 	}
-	*head = NULL'
+	*head = NULL;
 }
