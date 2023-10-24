7bac8d008ac415dc61948bc588a72b292b679d8c
Michael Gebregiorgis <you@example.com>
2023-10-24 05:47:10 -0700

    new node at the end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -16,7 +16,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *tmp = *head;
 
 	val = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (val == NULL)
 	{
 		return (NULL);
 	}
