5249d8df8fd4c986ddcc5be2457f0f617d790fd9
Michael Gebregiorgis <you@example.com>
2023-10-24 04:35:01 -0700

    number of node elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint)len - prints all the elements of a listint_t list
+ *
+ * @a: Data type
+ *
+ * Return: node element
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t val = 0;
+
+	while (a != NULL)
+	{
+		val++;
+		a = a->next;
+	}
+	return (val);
+}
