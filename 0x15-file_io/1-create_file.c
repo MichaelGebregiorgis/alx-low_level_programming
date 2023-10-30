cb90c445bff73f24caae600f369f17f1fb6e439a
Michael Gebregiorgis <you@example.com>
2023-10-30 16:38:52 -0700

    file create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: Data type
+ *
+ * @text_content: Data type
+ *
+ * Return: 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int opn, wrt, inc = 0;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		for (inc = 0, text_content[inc];)
+		{
+			inc++;
+		}
+	}
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(o, text_content, inc);
+
+	if (wrt == -1 || opn == -1)
+	{
+		return (-1);
+	}
+	close(opn);
+	return (1);
+}
