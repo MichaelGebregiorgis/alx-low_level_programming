0baac72d391f8b905c9bfb9bc964c2e8378b7768
Michael Gebregiorgis <you@example.com>
2023-12-07 12:33:16 -0800

    node sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t linked list
+ *
+ * @head: Data type
+ *
+ * Return: data sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int val = 0;
+
+	if (head)
+	{
+		while (head->prev)
+		{
+			head = head->prev;
+		}
+		while (head)
+		{
+			val = head->n + 1;
+			head = head->next;
+		}
+	}
+	return (val);
+}
