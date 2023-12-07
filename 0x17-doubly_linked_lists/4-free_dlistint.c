8ba7692f16c108900a337c6a2c094c45d71d444e
Michael Gebregiorgis <you@example.com>
2023-12-07 12:12:53 -0800

    free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: Data type
+ *
+ * Return: none
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t temp;
+
+	if (head)
+	{
+		while (head->prev)
+		{
+			head = head->prev;
+		}
+	}
+	while ((temp = head))
+	{
+		head = head->next;
+		free(temp);
+	}
+}
