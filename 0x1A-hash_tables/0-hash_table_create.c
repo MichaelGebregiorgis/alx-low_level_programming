b109406e87517d54fc5ef1d0a50b80c16d7c3b5c
Michael Gebregiorgis <you@example.com>
2023-12-22 12:37:59 -0800

    Hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Create hash table
+ *
+ * @size: Data type
+ *
+ * Return: ptr
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int inc;
+	hash_table_t *hash;
+
+	hash = malloc(sizeof(hash_table_t));
+	if (!hash)
+	{
+		return (NULL);
+	}
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash->array)
+	{
+		return (NULL);
+	}
+	while (inc < size)
+	{
+		hash->array[inc] = NULL;
+		inc++;
+	}
+	return (hash);
+}
