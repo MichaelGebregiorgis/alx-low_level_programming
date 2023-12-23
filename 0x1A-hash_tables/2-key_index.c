aba56c9facc15dcbbffd2af99cad74aefeb36a70
Michael Gebregiorgis <you@example.com>
2023-12-23 08:17:09 -0800

    indexer

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives you the index of a key
+ *
+ * @size: Data type
+ *
+ * @key: Data type
+ *
+ * Return: Alg
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
