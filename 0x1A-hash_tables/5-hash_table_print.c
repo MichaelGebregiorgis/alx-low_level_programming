ea5ce1051dacc8d7a522f512299841f3a0540ea2
Michael Gebregiorgis <you@example.com>
2023-12-23 09:43:38 -0800

    table print

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ *
+ * @ht: Data type
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int inc = 0;
+	hash_node_t *nd;
+	unsigned char flag;
+
+	flag = 0;
+	if (!ht)
+	{
+		return;
+	}
+	printf("{");
+	while (inc <ht->size)
+	{
+		if (ht->array[inc])
+		{
+			if (flag == 1)
+			{
+				printf(", ");
+			}
+			nd = ht->array[inc];
+			while (nd)
+			{
+				printf("'%s': '%s'", nd->key, nd->value);
+				nd = nd->next;
+				if (nd)
+					printf(", ");
+			}
+			flag = 1;
+		}
+		inc++;
+	}
+	printf("}\n");
+}
