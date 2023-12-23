cb9eb3ba046a9b6b158ed56a8b155aab258653fd
Michael Gebregiorgis <you@example.com>
2023-12-23 09:58:28 -0800

    Del

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * 
+ * @ht: Data type
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int inc = 0;
+	hash_node_t *nd *temp;
+	hash_table_t *hd = ht;
+
+	while (inc < ht->size)
+	{
+		if (ht->array[inc])
+		{
+			nd = ht->array[inc];
+			while (node)
+			{
+				tempo = node->next;
+				free(nd->key);
+				free(nd->value);
+				free(nd);
+				nd = tempo;
+			}
+		}
+		inc++;
+	}
+	free(hd->array);
+	free(hd);
+}
