a9f945863d0cc3f22e3e04e7058e26b1993cad3e
Michael Gebregiorgis <you@example.com>
2024-02-21 11:36:17 -0800

    parameter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!usr/bin/python3
+"""grid"""
+
+
+def island_perimeter(grid):
+    """return island perimeter"""
+    wid = len(grid[0])
+    hgt = len(grid)
+    bound = 0
+    size = 0
+
+    for h in range(height):
+        for w in range(width):
+            if grid[h][w] == 1:
+                size = size + 1
+                if (w > 0 and grid[h][w - 1] == 1):
+                    bound = bound + 1
+                if (h > 0 and grid[h - 1][w] == 1):
+                    bound = bound + 1
+    return (size * 4 - bound * 2)
